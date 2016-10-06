78f4ea962e257d67f44c3bb189ca6193d31cd28b
alaric <alaric@wullerot.ch>
2016-10-06 15:54:12 +0200

    minified js

---
+++ text_ckeditor/static/text_ckeditor/js/djangolink/dialogs/djangolink.src.js
@@ -0,0 +1,160 @@
+var DjangoLinkDialog = ( function( $ ) {
+    'use strict';
+
+    var iframe_html = '<iframe style="position:static; width:100%; height:100%; border:none;" />';
+
+    CKEDITOR.dialog.add('djangolink', djangolink_dialog );
+
+    function djangolink_dialog( instance ) {
+        var anchors;
+        var editor = instance;
+        var plugin = CKEDITOR.plugins.djangolink;
+        var iframeURL = editor.config.djangolinkIframeURL;
+        var verifyURL = editor.config.djangolinkVerifyURL;
+        var dialogInstance = {
+            onOk: on_ok,
+            onShow: on_show,
+            title: 'Link',
+            width: 800,
+            height: 400,
+            minHeight: 300,
+            contents: [ { elements: [ { type: 'html', html: iframe_html } ] } ]
+        };
+
+        function on_ok() {
+            var data = {};
+            var dialog = this;
+            var $iframe = $( dialog.parts.contents.$ ).find('iframe').contents();
+            var $form = $iframe.find('form');
+            var $fields = $form.find("input, select");
+            for ( var i = 0; i < $fields.length; i++ ) {
+                data[ $fields[ i ].name ] = $fields[ i ].value;
+            }
+            $.ajax( {
+                method: 'post',
+                url: verifyURL,
+                data: data,
+                success: success
+            } );
+
+            function success( response, status, xhr ) {
+                // django form error ------------------------------------------
+                if ( response.valid != 'true') {
+                    show_form_errors( $form, response );
+                    return false;
+                }
+                // no form errors ---------------------------------------------
+                var selection = editor.getSelection();
+                var element = dialog.getSelectedElement()
+                var attributes = plugin.getLinkAttributes( editor, response.data, response.link_value );
+                if ( !element ) {
+                    insert_link_element( editor, selection, attributes );
+                } else {
+                    edit_link_element( selection, element, attributes, data )
+                }
+                $( 'body', $iframe ).remove();
+                CKEDITOR.dialog.getCurrent().hide();
+            };
+
+            return false;
+        };
+
+        function on_show() {
+            var dialog = this;
+            var editor = this.getParentEditor();
+            var selection = editor.getSelection();
+            var element = plugin.getSelectedLink( editor );
+            if ( element && element.hasAttribute( 'href' ) ) {
+                if ( !selection.getSelectedElement() ) {
+                    selection.selectElement( element );
+                }
+            } else {
+                element = null;
+            }
+            // Record down the selected element in the dialog.
+            // TODO find a proper way to do this
+            dialog._.selectedElement = element;
+            var data = plugin.parseLinkAttributes( element );
+            var $dialog = $( dialog.parts.contents.$ );
+            var $iframe = $( 'iframe', $dialog );
+            style_dialog( $dialog );
+            $iframe.hide()
+                   .unbind( 'load' )
+                   .attr( { src: iframeURL + "?_popup=true&" + $.param( data ) } )
+                   .on('load', load_iframe );
+        };
+
+        return dialogInstance;
+    };
+
+    function style_dialog( $dialog ) {
+        $dialog.css( { 'padding': '0' } );
+        $( '.cke_dialog_page_contents, table[role=presentation]', $dialog ).css( { height: '100%' } );
+        $( '.cke_dialog_ui_vbox_child', $dialog ).css( { padding: 0, height: '100%' } );
+        $( '.cke_dialog_ui_vbox_child iframe', $dialog ).css( { display: 'block' } );
+    };
+
+    function load_iframe() {
+        var $iframe = $( this )
+        var $content = $iframe.contents();
+        $( 'html, body', $content ).scrollTop( 0 );
+        $( 'h1, .submit-row', $content ).hide().end();
+        $( '#content, #container', $content ).css({ minWidth: 0, padding: 0 });
+        $( 'form', $content ).on( 'submit', prevent_default );
+        $iframe.show();
+    };
+
+    function edit_link_element( selection, element, attributes, data ) {
+        var href = element.data( 'cke-saved-href' );
+        var textView = element.getHtml();
+
+        element.setAttributes( attributes.set );
+        element.removeAttributes( attributes.removed );
+
+        // Update text view when user changes protocol (#4612).
+        if ( href == textView || data.type == 'email' && textView.indexOf( '@' ) != -1 ) {
+            element.setHtml( data.type == 'email' ? data.email.address : attributes.set[ 'data-cke-saved-href' ] );
+            selection.selectElement( element );
+        }
+    }
+
+    function insert_link_element( editor, selection, attributes ) {
+        var range = selection.getRanges()[ 0 ];
+        if ( range.collapsed ) {
+            insert_link_text( editor, range ); // if Nothing is selected insert placeholder text
+        }
+        var style = new CKEDITOR.style( { element: 'a', attributes: attributes.set } );
+        style.type = CKEDITOR.STYLE_INLINE;
+        style.applyToRange( range, editor );
+        range.select();
+    };
+
+    function insert_link_text( editor, range) {
+        var text = new CKEDITOR.dom.text('link', editor.document );
+        range.insertNode(text);
+        range.selectNodeContents(text);
+    };
+
+    function show_form_errors( $form, data ) {
+        // clean old errors
+        $('.form-rows', $form).removeClass("errors");
+        $('.errorlist', $form).remove();
+        // attach new errors
+        for ( var i = 0; i < data.errors.length; i++ ) {
+            var name = data.errors[ i ].shift();
+            var $field = $( '.form-row.field-' + name, $form );
+            var html = '<ul class="errorlist">';
+            for ( var e = 0; e <  data.errors[ i ].length; e++ ) {
+                html += '<li>' +  data.errors[ i ][ e ] + '</li>';
+            }
+            html += '</ul>';
+            $field.addClass( 'errors' );
+            $field.prepend( html );
+        }
+    };
+
+    function prevent_default( e ) {
+        e.preventDefault();
+    };
+
+} )(django.jQuery);
---
+++ text_ckeditor/static/text_ckeditor/js/djangolink/plugin.src.js
@@ -0,0 +1,211 @@
+var DjangoLink = ( function( $ ) {
+    'use strict';
+
+    var conf = {
+        requires: 'dialog,fakeobjects',
+        icons: 'DjangoLink',
+        hidpi: true,
+        onLoad: on_load,
+        init: init,
+        editLink: edit_link,
+        afterInit: init_complete,
+    };
+
+    // Add Plugin to Editor
+    CKEDITOR.plugins.add( 'djangolink', conf );
+
+    // Set Plugin helpers
+    CKEDITOR.plugins.djangolink = {
+        getLinkAttributes: get_link_attributes,
+        getSelectedLink: get_selected_link,
+        parseLinkAttributes: parse_link_attributes,
+    };
+
+
+    function init( editor ) {
+        var plugin = this;
+        var allowed = 'a[!href, data-*]';
+        var required = 'a[href]';
+
+        CKEDITOR.dialog.add(
+            'djangolink',
+            this.path + 'dialogs/djangolink.js'
+        );
+        editor.addCommand( 'djangolink', {
+            allowedContent: allowed,
+            requiredContent: required,
+            exec: exec_djangolink
+        });
+        editor.ui.addButton( 'DjangoLink', {
+            label: editor.lang.link.toolbar,
+            command: 'djangolink',
+            toolbar: 'djangolinks,10',
+            icon: 'link'
+        } );
+        editor.on( 'doubleclick', select_link, null, null, 0 );
+
+        function exec_djangolink() {
+            var s = editor.getSelection();
+            var el = s.getSelectedElement() || s.getCommonAncestor().getAscendant('a', true);
+            plugin.editLink(el, editor);
+        };
+
+        function select_link( evt ) {
+            var element = CKEDITOR.plugins.link.getSelectedLink( editor ) || evt.data.element;
+            if ( !element.isReadOnly() ) {
+                if ( element.is( 'a' ) ) {
+                    // Pass the link to be selected along with event data.
+                    evt.data.dialog = 'djangolink';
+                    evt.data.link = element;
+                }
+            }
+            if ( evt.data.dialog in { djangolink: 1, anchor: 1 } && evt.data.link ) {
+                editor.getSelection().selectElement( evt.data.link );
+            }
+        };
+    };
+
+    function on_load( ) {
+        // Add the CSS styles for anchor placeholders.
+        var iconPath = CKEDITOR.getUrl( this.path + 'images' + ( CKEDITOR.env.hidpi ? '/hidpi' : '' ) + '/anchor.png' ),
+            baseStyle = 'background:url(' + iconPath + ') no-repeat %1 center;border:1px dotted #00f;background-size:16px;';
+
+        var template = '.%2 a.cke_anchor,' +
+            '.%2 a.cke_anchor_empty' +
+            ',.cke_editable.%2 a[name]' +
+            ',.cke_editable.%2 a[data-cke-saved-name]' +
+            '{' +
+                baseStyle +
+                'padding-%1:18px;' +
+                // Show the arrow cursor for the anchor image (FF at least).
+                'cursor:auto;' +
+            '}' +
+            '.%2 img.cke_anchor' +
+            '{' +
+                baseStyle +
+                'width:16px;' +
+                'min-height:15px;' +
+                // The default line-height on IE.
+                'height:1.15em;' +
+                // Opera works better with "middle" (even if not perfect)
+                'vertical-align:text-bottom;' +
+            '}';
+
+        // Styles with contents direction awareness.
+        function cssWithDir( dir ) {
+            return template.replace( /%1/g, dir == 'rtl' ? 'right' : 'left' ).replace( /%2/g, 'cke_contents_' + dir );
+        }
+
+        CKEDITOR.addCss( cssWithDir( 'ltr' ) + cssWithDir( 'rtl' ) );
+    };
+
+    function edit_link(element, editor) {
+        var $body = $('body');
+        var that = this;
+        editor.openDialog('djangolink');
+    };
+
+    function init_complete( editor ) {
+        // init complete
+    };
+
+
+    // Plugin Helpers ---------------------------------------------------------
+
+    function get_link_attributes(editor, data, link_value) {
+        /**
+         * Converts link data produced by {@link #parseLinkAttributes} into an object which consists
+         * of attributes to be set (with their values) and an array of attributes to be removed.
+         * This method can be used to compose or to update any link element with the given data.
+         *
+         * @since 4.4
+         * @param {CKEDITOR.editor} editor
+         * @param {Object} data Data in {@link #parseLinkAttributes} format.
+         * @returns {Object} An object consisting of two keys, i.e.:
+         *
+         *        {
+         *            // Attributes to be set.
+         *            set: {
+         *                href: 'http://foo.bar',
+         *                target: 'bang'
+         *            },
+         *            // Attributes to be removed.
+         *            removed: [
+         *                'id', 'style'
+         *            ]
+         *        }
+         *
+         */
+        var set = {'data-djangolink': true};
+        var removed;
+        var excludes = ['_popup', '_save', 'csrfmiddlewaretoken'];
+
+        $.each(data, function(index, item) {
+            if ($.inArray(index, excludes) < 0 && item != null) {
+                set['data-' + index] = item;
+            }
+        });
+        set.href = '';
+        if (link_value) {
+            set.href = link_value;
+        }
+        removed = [];
+
+        return {
+            set: set,
+            // removed: CKEDITOR.tools.objectKeys( removed ) what is this?
+            removed: CKEDITOR.tools.objectKeys( removed )
+        };
+    };
+
+    function get_selected_link( editor ) {
+        var selection = editor.getSelection();
+        var selectedElement = selection.getSelectedElement();
+        if ( selectedElement && selectedElement.is( 'a' ) ) {
+            return selectedElement;
+        }
+        var range = selection.getRanges()[ 0 ];
+        if ( range ) {
+            range.shrink( CKEDITOR.SHRINK_TEXT );
+            return editor.elementPath( range.getCommonAncestor() ).contains( 'a', 1 );
+        }
+        return null;
+    };
+
+    function parse_link_attributes( element ) {
+        /**
+        * Parses attributes of the link element and returns an object representing
+        * the current state (data) of the link. This data format is a plain object accepted
+        * e.g. by the Link dialog window and {@link #getLinkAttributes}.
+        *
+        * **Note:** Data model format produced by the parser must be compatible with the Link
+        * plugin dialog because it is passed directly to {@link CKEDITOR.dialog#setupContent}.
+        *
+        * @since 4.4
+        * @param {CKEDITOR.editor} editor
+        * @param {CKEDITOR.dom.element} element
+        * @returns {Object} An object of link data.
+        **/
+        if (!element || !element.$.attributes) {
+            return {};
+        }
+        var data = {};
+        $.each(element.$.attributes, function(index, attribute) {
+            var key = attribute.name.substr('data-'.length);
+            data[key] = attribute.value;
+        });
+        return data
+    };
+
+
+    // Utilities --------------------------------------------------------------
+
+    function unescapeSingleQuote( str ) {
+        return str.replace( /\\'/g, '\'' );
+    };
+
+    function escapeSingleQuote( str ) {
+        return str.replace( /'/g, '\\$&' );
+    };
+
+})( django.jQuery );
