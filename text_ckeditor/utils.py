7cc25be6d218a9dd6de9c9a77b8310546415ebc7
alaric <alaric@wullerot.ch>
2016-10-26 10:52:35 +0200

    debug templatetag -> email

--- text_ckeditor/utils.py
+++ text_ckeditor/utils.py
@@ -60,7 +60,11 @@ class CKEditorHtml(object):
                 href = obj.get_email()
             else:
                 href = href.replace('mailto:', '')
-            mail = mail_to_js(href, link_text=link.text, css_class=css_class)
+            if link.text:
+                text = link.text
+            else:
+                text = href
+            mail = mail_to_js(href, link_text=text, css_class=css_class)
             link_new = fragment_fromstring(mail)
             link.addnext(link_new)
             link.getparent().remove(link)
@@ -75,33 +79,32 @@ class CKEditorHtml(object):
 def mail_to_js(email, *args, **kwargs):
     result = ''
     text = kwargs.get('link_text', email)
-    if text:
-        css_class =  kwargs.get('css_class', '')
-        email_array_content = ''
-        text_array_content = ''
-        r = lambda c: '"' + str(ord(c)) + '",'
-        for c in email:
-            email_array_content += r(c)
-        for c in text:
-            text_array_content += r(c)
-        id = "_tyjsdfss-" + str(random.randint(1000, 999999999999999999))
-        re_email = re.sub(r',$', '', email_array_content)
-        re_text = re.sub(r',$', '', text_array_content)
-        result = ('<span id="%s"><script>'
-                  'var _tyjsdf=[%s];'
-                  'var _qplmks=[%s];'
-                  'var content=('
-                  '\'<a class="%s" '
-                  'href="&#x6d;&#97;&#105;&#x6c;&#000116;&#111;&#x3a;\''
-                  ');'
-                  'for(_i=0;_i<_tyjsdf.length;_i++){'
-                  'content+=("&#"+_tyjsdf[_i]+";");'
-                  '}'
-                  'content+=(\'">\');'
-                  'for(_i=0;_i<_qplmks.length;_i++){'
-                  'content+=(\'&#\'+_qplmks[_i]+\';\');'
-                  '}'
-                  'content+=(\'</a>\');'
-                  'document.getElementById(\'%s\').innerHTML=content;'
-                  '</script></span>' %(id, re_email, re_text, css_class, id))
+    css_class =  kwargs.get('css_class', '')
+    email_array_content = ''
+    text_array_content = ''
+    r = lambda c: '"' + str(ord(c)) + '",'
+    for c in email:
+        email_array_content += r(c)
+    for c in text:
+        text_array_content += r(c)
+    id = "_tyjsdfss-" + str(random.randint(1000, 999999999999999999))
+    re_email = re.sub(r',$', '', email_array_content)
+    re_text = re.sub(r',$', '', text_array_content)
+    result = ('<span id="%s"><script>'
+              'var _tyjsdf=[%s];'
+              'var _qplmks=[%s];'
+              'var content=('
+              '\'<a class="%s" '
+              'href="&#x6d;&#97;&#105;&#x6c;&#000116;&#111;&#x3a;\''
+              ');'
+              'for(_i=0;_i<_tyjsdf.length;_i++){'
+              'content+=("&#"+_tyjsdf[_i]+";");'
+              '}'
+              'content+=(\'">\');'
+              'for(_i=0;_i<_qplmks.length;_i++){'
+              'content+=(\'&#\'+_qplmks[_i]+\';\');'
+              '}'
+              'content+=(\'</a>\');'
+              'document.getElementById(\'%s\').innerHTML=content;'
+              '</script></span>' %(id, re_email, re_text, css_class, id))
     return mark_safe(result)
