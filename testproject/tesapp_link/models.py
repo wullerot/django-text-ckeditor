d8234c572a8199821367c39670fdc46434703142
alaric <alaric@wullerot.ch>
2019-07-16 12:05:42 +0200

    abstract link class

--- testproject/tesapp_link/models.py
+++ testproject/tesapp_link/models.py
@@ -1,8 +1,6 @@
 from __future__ import unicode_literals
 
-from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
 
 from text_ckeditor.text_ckeditor_links.models import AbstractLink
 
