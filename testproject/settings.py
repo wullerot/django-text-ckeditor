dd8f86a6ffcbde8d1b85fc6e70d2653dd65b2737
alaric mägerle <alaric@wullerot.ch>
2021-01-03 14:33:19 +0100

    cleanup

--- testproject/settings.py
+++ testproject/settings.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.append(BASE_DIR)
@@ -10,14 +10,10 @@ sys.path.append(os.path.join(BASE_DIR, '..'))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'testproject', 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'testproject', 'static')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%&9@q+$c*9s##ss#@0(s!8_kjhkj987,mnbkjhkjh11yjum_m&5_nak14q-+b'
+SECRET_KEY = 'testsecretkey'
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
